4d1b7ccd72301dc4ebb809a43f6ac6971440c527
Mohammed JABRI <mohammed.jabri2000@gmail.com>
2020-07-09 20:22:48 +0100

    Dynamic number of shapes

--- js/app.js
+++ js/app.js
@@ -3,32 +3,48 @@ let inputs = document.querySelectorAll('.heightText');
 
 const heightRef = document.querySelector('.shapeImg').height;
 
-console.log(heightRef);
+let nbrChange = (input) => {
 
-sliders.forEach(slider => {
-    slider.addEventListener('change', ()=>{
+    let value = input.value;
+
+    let items = document.querySelectorAll('.item');
+
+    if(value == items.length) {
+
+        return;
+
+    } else if(value > items.length) {
+
+        items[items.length - 1].parentElement.appendChild(items[items.length - 1].cloneNode(true));
+        nbrChange(input);
+
+    } else if(value < items.length) {
         
-        let shapeImg = slider.parentElement.parentElement.querySelector('.shapeImg');
-        let heightText = slider.parentElement.querySelector('.heightText');
+        items[items.length - 1].parentElement.removeChild(items[items.length - 1]);
+        nbrChange(input);
+    }
+}
 
-        heightText.value = slider.value;
+let rangeChange = (slider) => {
 
-        let height = (slider.value * 100 / slider.max) * heightRef / 100;
-        shapeImg.style.height = height + "px";
+    let shapeImg = slider.parentElement.parentElement.querySelector('.shapeImg');
+    let heightText = slider.parentElement.querySelector('.heightText');
 
-    });
-});
+    heightText.value = slider.value;
 
-inputs.forEach(input => {
-    input.addEventListener('change', ()=>{
+    let height = (slider.value * 100 / slider.max) * heightRef / 100;
+    shapeImg.style.height = height + "px";
 
-        let shapeImg = input.parentElement.parentElement.querySelector('.shapeImg');
-        let slider = input.parentElement.querySelector('.heightRange');
+}
 
-        slider.value = input.value;
+let textChange = (input) => {
 
-        let height = (input.value * 100 / input.max) * heightRef / 100;
-        shapeImg.style.height = height + "px";
+    let shapeImg = input.parentElement.parentElement.querySelector('.shapeImg');
+    let slider = input.parentElement.querySelector('.heightRange');
 
-    });
-});
+    slider.value = input.value;
+
+    let height = (input.value * 100 / input.max) * heightRef / 100;
+    shapeImg.style.height = height + "px";
+
+}
